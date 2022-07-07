a5a7631a477e409e94eb4011b93b4e35b126e828
riverback <hhz_pku@163.com>
2022-07-08 07:42:33 +0800

    utils

--- utils/pyutils.py
+++ utils/pyutils.py
@@ -1,6 +1,8 @@
 import random
 import torch
 import numpy as np
+import os
+from torch.backends import cudnn
 
 def dumpclean(obj: dict):
     """To print a dictionary line by line"""
@@ -19,6 +21,12 @@ def dumpclean(obj: dict):
                 print(v)
     else:
         print(obj)
+        
+def print_config(config):
+    print("Config Information:")
+    config_list = config._get_kwargs()
+    for arg in config_list:
+        print(arg)
                 
                 
 def set_seed(seed):
@@ -27,6 +35,11 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     random.seed(seed)
     np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = True
+    cudnn.benchmark = True
 
 
 def print_metrics(metrics: dict, mode='train'):
@@ -35,13 +48,14 @@ def print_metrics(metrics: dict, mode='train'):
     
     for metric in metrics.keys():
         print("{}: (threshold, {})".format(metric, metric))
-        for k, v in metric:
+        for k, v in metrics[metric].items():
             print("({} {:.4f})".format(k, v), end=' ')
-            if v > best_metrics[metrics][1]:
-                best_metrics[metrics][0] = f'threshold-{k}'
-                best_metrics[metrics][1] = v
+            if v > best_metrics[metric][1]:
+                best_metrics[metric][0] = f'threshold-{k}'
+                best_metrics[metric][1] = v
+        print()
                 
     if mode != 'train':
         print("\nBest metrics:")
-        for k, v in best_metrics:
+        for k, v in best_metrics.items():
             print("{}: {:.4f} use threshold {}".format(k, v[1], v[0]))
