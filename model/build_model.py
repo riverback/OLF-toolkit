a5d55337ed915b8da14788c9a2caeea6651fa470
riverback <hhz_pku@163.com>
2022-07-21 22:45:27 +0800

    add classifaction model build method

--- model/build_model.py
+++ model/build_model.py
@@ -3,7 +3,9 @@ from model.Semantic_Segmentation.U_Net.unet import U_Net, AttU_Net
 from model.Semantic_Segmentation.DeepLab.deeplabv3 import deeplabv3_resnet50, deeplabv3_resnet101
 from model.Semantic_Segmentation.TransUNet.model.transunet import build_transunet
 
-MODEL_LIST = ['U_Net', 'DeepLabV3_ResNet50', 'DeepLabV3_ResNet101', 'TransUNet', 'AttU_Net']
+from model.Backbone.resnet import resnet50, resnet101, resnet152
+
+MODEL_LIST = ['U_Net', 'DeepLabV3_ResNet50', 'DeepLabV3_ResNet101', 'TransUNet', 'AttU_Net', 'ResNet50', 'ResNet101', 'ResNet152']
 
 def build_model(config) -> nn.Module:
     if config.model not in MODEL_LIST:
@@ -21,6 +23,12 @@ def build_model(config) -> nn.Module:
         model = deeplabv3_resnet101(config.image_channels, config.output_channels)
     elif config.model == 'TransUNet':
         model = build_transunet(config.vit_name, n_classes=config.output_channels)
+    elif config.model == 'ResNet50':
+        model = resnet50(config.image_channels, config.output_channels)
+    elif config.model == 'ResNet101':
+        model = resnet101(config.image_channels, config.output_channels)
+    elif config.model == 'ResNet152':
+        model = resnet152(config.image_channels, config.output_channels)
     else:
         ...
         
