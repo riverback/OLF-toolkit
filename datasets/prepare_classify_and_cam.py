640cbeff298b822a9706e008bbef292405d299f2
riverback <hhz_pku@163.com>
2022-07-27 15:45:29 +0800

    generate pictures

--- datasets/prepare_classify_and_cam.py
+++ datasets/prepare_classify_and_cam.py
@@ -3,12 +3,13 @@
 """
 
 import os
+from typing import List
 import numpy as np
 import cv2
 import nibabel as nib
 from os.path import join as ospj
-# from dcmutils import load_scan, normalize_minmax, get_pixels_hu
-from datasets.dcmutils import load_scan, normalize_minmax, get_pixels_hu
+from dcmutils import load_scan, normalize_minmax, get_pixels_hu
+# from datasets.dcmutils import load_scan, normalize_minmax, get_pixels_hu
 
 
 def ReadDcmSequence_XY_Pydicom(dcm_folder, norm=True):
@@ -130,12 +131,89 @@ def resize_ori_image_XY(dcm_folder, label_path, DO_Label_Value=[], folder='001')
 
 
 
-
+def generate_class2_png_and_label(label_path, DO_Label_Value, output_folder='Data/Classify_YZ'):
+    """
     
+    """
+    annotations_path = os.path.join(output_folder, 'annotations.txt')
+    png_template = 'IMG{:05d}.png'
+    png_folder = os.path.join(output_folder, 'PNG_YZ')
+    olf_folder = os.path.join(png_folder, 'olf')
+    do_folder = os.path.join(png_folder, 'do')
+    
+    if not os.path.exists(png_folder):
+        os.makedirs(png_folder)
+    if not os.path.exists(olf_folder):
+        os.makedirs(olf_folder)
+    if not os.path.exists(do_folder):
+        os.makedirs(do_folder)
+
+    index = 0
+    f = open(annotations_path, 'a')
+    for i in range(len(label_path)):
+        volume_path = label_path[i]
+        volume = nib.load(volume_path)
+        volume_data = volume.get_fdata()
+
+        volume_data = np.flip(volume_data, 2)
+        l, w, h = volume_data.shape
+
+        for x in range(l):
+            img = volume_data[x, :, :]
+            if img.max() < 0.5:
+                continue
+            for value in np.unique(img).tolist():
+                if value < 0.5:
+                    continue
+                patch = np.zeros([64, 64])
+                img_temp = np.where(img==value, 1, 0)
+                axis0, axis1 = np.where(img_temp==1)
+                axis0_min, axis0_max = axis0.min(), axis0.max()
+                axis0_gap = axis0_max - axis0_min
+                axis1_min, axis1_max = axis1.min(), axis1.max()
+                axis1_gap = axis1_max - axis1_min
+
+                if axis0_min+64 > img.shape[0]:
+                    axis0_min = img.shape[0] - 64
+                elif axis0_gap < 50:
+                    axis0_min -= 25
+                else:
+                    axis0_min -= 2
+
+                if axis1_min+64 > img.shape[1]:
+                    axis1_min = img.shape[1] - 64
+                elif axis1_gap < 50:
+                    axis1_min -= 25
+                else:
+                    axis1_gap -= 2
+
+
+                patch = img_temp[axis0_min:axis0_min+64, axis1_min:axis1_min+64]
+                if value in DO_Label_Value[i]:
+                    label = 1
+                    png_path = os.path.join(do_folder, png_template.format(index))
+                else:
+                    label = 0
+                    png_path = os.path.join(olf_folder, png_template.format(index))
+
+                patch = patch * 255
+                # patch = np.rot90(np.flipud(patch))
+
+                cv2.imwrite(png_path, patch)
+                f.write('{} {}\n'.format(png_path, label))
+                f.flush()
+                index += 1
+
+    f.close()
+
+    print("finished")   
+
+                
+
 
 
 if __name__ == '__main__':
-
+    '''
     DO_Spine = [10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0]
     label_path = 'Data/GT/001/Label.nii.gz'
     dcm_folder = 'Data/RAW/001'
@@ -150,6 +228,8 @@ if __name__ == '__main__':
     label_classify_xy_path = '002.txt'
     # resize_ori_image_XY(dcm_folder, label_path, DO_Spine, '002')
     # generate_class3_label(dcm_folder, label_path, label_classify_xy_path, DO_Spine)
-    
-   
+    '''
+
+    generate_class2_png_and_label(['Data/GT/001/Label.nii.gz', 'Data/GT/002/Label.nii.gz'], [[10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0], [18.0]], 'Data/Classify_YZ')
+
 
